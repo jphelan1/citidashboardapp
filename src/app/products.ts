3384c6fc010174f7b0fac7b98ea2e000606a59f1
Jessica Phelan <jmp07945@msn.com>
2020-08-04 16:09:42 -0400

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,19 @@
+export const products = [
+  {
+    name: 'Resources',
+    //price: 799,
+    description: 'A look at resources to help  you'
+  },
+  {
+    name: 'Short Term Action Plan',
+    //price: 699,
+    description: 'Short Term Plan'
+  },
+  {
+    name: 'Long Term Plan',
+    //price: 299,
+    description: 'Long term plan'
+  }
+];
+
+
